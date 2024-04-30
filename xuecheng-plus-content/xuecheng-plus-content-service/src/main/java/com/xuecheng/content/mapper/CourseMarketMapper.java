0d0c1afd516fd147c2a1735008f1b92b37ead65c
du-haoyi <2570571063@qq.com>
2024-04-30 09:15:52 +0800

    Initial commit

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseMarketMapper.java
@@ -0,0 +1,17 @@
+package com.xuecheng.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.po.CourseMarket;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 课程营销信息 Mapper 接口
+ * </p>
+ *
+ * @author cyborg2077
+ */
+public interface CourseMarketMapper extends BaseMapper<CourseMarket> {
+
+}
